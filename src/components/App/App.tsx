d55ab10652983e174372e57aaa47dcb70b71289e
Elizabeth Shkinder <elizabethshkinder@unknown.home>
2026-01-20 11:46:55 +0000

    create project components folders and files

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import CafeInfo from '../CafeInfo/CafeInfo';
+
+import css from './App.module.css'
+
+export default function App() {
+  
+
+  return (
+    <div>
+      <div className={css.app}></div>
+    </div>
+  )
+}
